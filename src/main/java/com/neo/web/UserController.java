57b6253401360c6471b837056eb7f90a7f832d47
MR.Song <82398485@163.com>
2018-09-30 17:09:56 +0800

    2datasource of mybatis use springboot

---
+++ src/main/java/com/neo/web/UserController.java
@@ -0,0 +1,53 @@
+package com.neo.web;
+
+import java.util.List;
+
+import com.neo.mapper.test1.User1Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.neo.entity.UserEntity;
+import com.neo.mapper.test2.User2Mapper;
+
+@RestController
+public class UserController {
+
+    @SuppressWarnings("SpringJavaAutowiringInspection")
+    @Autowired
+    private User1Mapper user1Mapper;
+
+    @SuppressWarnings("SpringJavaAutowiringInspection")
+	@Autowired
+	private User2Mapper user2Mapper;
+	
+	@RequestMapping("/getUsers")
+	public List<UserEntity> getUsers() {
+		List<UserEntity> users=user1Mapper.getAll();
+		return users;
+	}
+	
+    @RequestMapping("/getUser")
+    public UserEntity getUser(Long id) {
+    	UserEntity user=user2Mapper.getOne(id);
+        return user;
+    }
+    
+    @RequestMapping("/add")
+    public void save(@RequestBody UserEntity user) {
+        user2Mapper.insert(user);
+    }
+    
+    @RequestMapping(value="update")
+    public void update(@RequestBody UserEntity user) {
+        user2Mapper.update(user);
+    }
+    
+    @RequestMapping(value="/delete/{id}")
+    public void delete(@PathVariable("id") Long id) {
+        user1Mapper.delete(id);
+    }
+    
+}
